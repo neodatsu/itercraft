8883f930186b953dfa88e7079bd6ebf244819b42
Laurent FERRER <laurent.ferrer@me.com>
2026-01-30 09:23:39 +0100

    fix for sonarcov

--- itercraft_front/vite.config.ts
+++ itercraft_front/vite.config.ts
@@ -15,5 +15,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 })
