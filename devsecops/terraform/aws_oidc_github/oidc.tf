a6fe284adc6f2b89828dea9f4277665e3e30cf45
Laurent FERRER <laurent.ferrer@me.com>
2026-01-30 17:04:26 +0100

    fiw aws oidc github

---
+++ devsecops/terraform/aws_oidc_github/oidc.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# GitHub OIDC Identity Provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+}
+
+# IAM Role assumable by GitHub Actions
+resource "aws_iam_role" "github_actions_ecr" {
+  name = "github-actions-ecr-push"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_repo}:ref:refs/tags/*",
+              "repo:${var.github_repo}:environment:itercraft"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Policy: push + pull ECR images
+resource "aws_iam_role_policy" "ecr_push" {
+  name = "ecr-push-policy"
+  role = aws_iam_role.github_actions_ecr.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "arn:aws:ecr:${var.aws_region}:${var.account_id}:repository/itercraft_*"
+      }
+    ]
+  })
+}
+
+output "role_arn" {
+  description = "ARN du role IAM pour GitHub Actions"
+  value       = aws_iam_role.github_actions_ecr.arn
+}
